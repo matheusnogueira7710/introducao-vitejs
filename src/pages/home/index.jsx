9da7a2e0360fe4e082f4fe0b44b2102aa7b2e3a4
matheusnogueira7710 <matheusnogueiraarrudaaa@gmail.com>
2026-03-13 14:41:10 -0300

    13 /03

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,21 @@
+
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router";
+
 function home () {
+
+const navigate = useNavigate();
+
     return(
-        <div>
+        <div className="conteinerHome">
             <h1>Aula de React com Vite JS</h1>
+
+            <button onClick={() => navigate('/exemplo/1')}>
+                exemplo 1
+
+            </button>
+
+            <Link to="/exemplo/2"> exemplo 2 </Link>
           
         </div>
 
