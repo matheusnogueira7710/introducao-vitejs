7d1d8ac936107c3b5e3133b04045b6b4c12f3d06
matheusnogueira7710 <matheusnogueiraarrudaaa@gmail.com>
2026-03-24 11:45:33 -0300

    24/03

--- src/pages/atividades/atv-01/index.jsx
+++ src/pages/atividades/atv-01/index.jsx
@@ -1,10 +1,22 @@
+
+import styles from './index.module.css';
+
 function atividade01() {
+    
     return (
+        <div className={styles.container}>
         <>
+        
             <h1>atividade 1</h1>
             <p>Conteúdo mínimo para componente React</p>
         </>
+        </div>
+
+        
+
+
     )
+    
 }
 
 export default atividade01;
